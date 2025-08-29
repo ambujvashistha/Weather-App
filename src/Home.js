038091222f6bc93eb514bc525ab02bd48732f56c
ambuj vashistha <ambujva123@gmail.com>
2025-08-29 16:46:29 +0530

    permission for location is wrking

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { View, SafeAreaView } from "react-native";
+import { View, SafeAreaView, Button } from "react-native";
 import Badge from "./components/badge/Badge";
 import CurrentLocation from "./module/CurrentLocation";
 import { styles } from "./HomeStyles";
 import SearchedLocation from "./module/SearchedLocation";
+import * as Location from "expo-location";
 
 // ? Top badge items which will help us to conditionally render top menu items
 const BADGE_ITEM = [
@@ -25,9 +26,27 @@ export default function Home() {
   const onPress = () => {
     // TODO
   };
+  async function loadData() {
+    console.log("Load data");
+    // TODO: Request for location permission
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    console.log("status", status);
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
+      <Button
+        title="Ask for Permission"
+        style={{ height: 20, width: 70, borderColor: "Black" }}
+        onPress={loadData}
+      />
       <View style={styles.badgeContainer}>
         {BADGE_ITEM.map((item) => {
           return (
--- src/module/CurrentLocation.js
+++ src/module/CurrentLocation.js
@@ -1,4 +1,4 @@
-import { ScrollView, ActivityIndicator, TextInput } from "react-native";
+import { ScrollView, ActivityIndicator, TextInput, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 import CurrentWeatherCard from "../components/current-weather-card/CurrentWeatherCard";
 import { styles } from "./ModuleStyles";
@@ -9,24 +9,50 @@ export default function CurrentLocation() {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const fetchCurrent = async (lat, lon) => {
-    // TODO: Call weather app api to get current weather info
-  };
-
-  useEffect(() => {
-    async function loadData() {
-      // TODO: Request for location permission
+  async function loadData() {
+    // TODO: Request for location permission
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
     }
 
-    loadData();
-  }, []);
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location);
+    setLocation(location);
+  }
+  const fetchCurrent = async (lat, lon) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
+    )
+      .then((res) => res.json)
+      .then((data) => setWeatherData(data));
+  };
+
+  // useEffect(() => {
+  //   async function loadData() {
+  //     // TODO: Request for location permission
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== "granted") {
+  //       setErrorMsg("Permission to access location was denied");
+  //       return;
+  //     }
+
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     console.log(location);
+  //     setLocation(location);
+  //   }
+  //   loadData();
+  // }, []);
 
   if (loading) return <ActivityIndicator size="large" />;
 
   return (
     <ScrollView style={styles.scrollView}>
-      <CurrentWeatherCard weatherData={weatherData} />
-      <TextInput onChangeText={() => {}} style={styles.input} />
+      <Button
+        title="Ask for Permission"
+        style={{ height: 20, width: 70, borderColor: "Black" }}
+      />
     </ScrollView>
   );
 }
